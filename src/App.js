e9a17ffc88b324abbbe3a5a6fe73a2a1a7ced554
Lian Guevara <liann.guevara@gmail.com>
2021-10-08 19:45:43 -0500

    view sale update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router} from "react-router-dom";
 import Home from "./Pages";
 import MaestroVenta from "./Pages/SaleMaster/index";
 import AgregarVenta from "./Pages/SaleAdd/index";
+import EditarVenta from "./Pages/SaleUpdate/index";
 import './App.css';
 
 
@@ -11,7 +12,7 @@ function App() {
     <>
     <Router>
     {/* <Home /> */}
-      <AgregarVenta />
+      <EditarVenta />
     </Router>
           </>
   );
