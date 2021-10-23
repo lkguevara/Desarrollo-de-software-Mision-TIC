9576572eaba42a94e29d8d05cb47803cfcd57619
Luisa Vidal <90484155+lvidala@users.noreply.github.com>
2021-10-23 01:01:04 -0500

    Actualizar privateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
 
 export const PrivateRoute = ({children}) => {
-    const { isAuthenticated, isLoading, getAcessTokenSilently} = useAuth0();
+    const { isAuthenticated, isLoading} = useAuth0();
 
     if(isLoading) return <div>Loading...</div>
 }
